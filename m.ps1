1f681421e92a445d4126ed72eb47f6ac35202738
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:56:47 +0000

    Backup Commit

--- m.ps1
+++ m.ps1
@@ -1,7 +1,9 @@
 $n  read-host
-
-write-host "&n""-""$b""-""$c"
-$m $mn read-host
+$b  read-host
+$c  read-host
+write-host "$n-$b-$c"
+$m  read-host
+$mn read-host
 $d=$m+$mn
 if(d -gt 70{
     write-post "Pass"
